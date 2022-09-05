2ba668c20949bf7d7d3b88ec47c857bd003eeef7
rasyaibnu55 <112846578+rasyaibnu55@users.noreply.github.com>
2022-09-04 23:12:12 -0700

    Add files via upload

---
+++ Python6-XTKJ3.py
@@ -0,0 +1,19 @@
+# Dolar ke rupiah
+
+dolar = int(input("masukan nilai dolar: "))
+rupiah = dolar * 14973
+print   ("dolar ke rupiah"  , rupiah)
+
+
+# Euro ke won
+
+euro = int(input("masukan nilai Euro:"))
+won = euro * 1357
+print ("euro ke won" , won)
+
+
+# yen ke rupiah
+
+yen =int(input("masukan nilai yen:"))
+Rupiah = yen * 106
+print ("Yen ke rupiah" , rupiah)
